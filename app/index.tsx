363e8786bf873d87e470d2c71ea8064335356c6d
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-09-18 09:56:47 -0300

    Altera StatusBar para a do react-native

---
+++ app/index.tsx
@@ -0,0 +1,88 @@
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function index() {
+  return (
+    <SafeAreaView style={estilos.container}>
+      <StatusBar style="auto"></StatusBar>
+      <View style={estilos.viewLogo}>
+        <Image source={require("../assets/dahora.png")} style={estilos.logo} />
+        <Text style={estilos.tituloApp}>Dá Hora Filmes</Text>
+      </View>
+      <View style={estilos.viewBotoes}>
+        <Pressable style={estilos.botaoInicial}>
+          <Ionicons name="search" size={24} color="white" />
+          <Text style={estilos.textoBotao}>Buscar Filmes</Text>
+        </Pressable>
+        <Pressable style={estilos.botaoInicial}>
+          <Ionicons name="star" size={24} color="gold" />
+          <Text style={estilos.textoBotao}>Favoritos</Text>
+        </Pressable>
+      </View>
+      <View style={estilos.viewRodape}>
+        <Pressable style={estilos.botaoRodape}>
+          <Ionicons name="lock-closed" size={18} color="white" />
+          <Text style={estilos.textoBotao}> Privacidade</Text>
+        </Pressable>
+        <Pressable style={estilos.botaoRodape}>
+          <Ionicons name="information-circle" size={18} color="white" />
+          <Text style={estilos.textoBotao}> Sobre</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  viewLogo: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 128,
+    height: 128,
+  },
+  tituloApp: {
+    fontSize: 32,
+    color: "#5451a6",
+    fontFamily: "Monoton",
+  },
+  viewBotoes: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  botaoInicial: {
+    backgroundColor: "#5451a6",
+    padding: 16,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textoBotao: {
+    color: "white",
+  },
+  viewRodape: {
+    backgroundColor: "#5451a6",
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  botaoRodape: {
+    padding: 16,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
