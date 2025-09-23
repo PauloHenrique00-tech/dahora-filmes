07a6ac5954b2f5c1d3768c2cd402f75a3fca32f8
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-09-23 10:56:39 -0300

    Cria app config com ENV da api key

---
+++ app.config.ts
@@ -0,0 +1,34 @@
+// Módulo responsável pelo acesso à dados de variáveis de ambiente
+import "dotenv/config";
+import { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "DáHora-Filmes",
+  slug: "dahora-filmes",
+  scheme: "dahora",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icone.png",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/splash-dahora.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/icone.png",
+      backgroundColor: "#000000",
+    },
+    edgeToEdgeEnabled: true,
+  },
+  plugins: ["expo-font", "expo-asset", "expo-router"],
+  // Acessando a API_KEY através do dotenv e guardando na chave apiKey
+  extra: {
+    apiKey: process.env.API_KEY,
+  },
+});
