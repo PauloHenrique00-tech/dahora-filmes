33fb298fad293ad2562f2fef3db9f4bc380014d4
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-10-02 11:11:35 -0300

    Altera texto Sobre e cor dos botões

--- app/index.tsx
+++ app/index.tsx
@@ -44,7 +44,7 @@ export default function index() {
           <Link href="/sobre" asChild>
             <Pressable style={estilos.botaoRodape}>
               <Ionicons name="information-circle" size={18} color="white" />
-              <Text style={estilos.textoBotao}> Sobre</Text>
+              <Text style={estilos.textoBotao}> Sobre o app</Text>
             </Pressable>
           </Link>
         </View>
@@ -81,7 +81,7 @@ const estilos = StyleSheet.create({
     alignItems: "center",
   },
   botaoInicial: {
-    backgroundColor: "#5451a6",
+    backgroundColor: "#222",
     padding: 16,
     borderRadius: 8,
     flexDirection: "row",
