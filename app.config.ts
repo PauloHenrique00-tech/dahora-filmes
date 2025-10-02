49b5bcda2f4e81c9c9169e2102d373cd93c6b23e
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-10-02 09:21:57 -0300

    Adiciona novas props ao app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "DáHora-Filmes",
   slug: "dahora-filmes",
+  owner: "ph00", // Necessário para o EAS
   scheme: "dahora",
   version: "1.0.0",
   orientation: "portrait",
@@ -30,6 +31,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   // Acessando a API_KEY através do dotenv e guardando na chave apiKey
   extra: {
     apiKey: process.env.API_KEY,
+    // Necessário para o EAS
     eas: {
       projectId: "632b4c17-d365-4afb-8f46-6b916150b6c5",
     },
